c220f0fab71c3773f1d603f5859f0b9561e5c7a9
borisdoginza <veridenis23@gmail.com>
2026-05-04 12:10:02 +0300

    Updates

--- assets/index-5TzHUFvf.js
+++ assets/index-5TzHUFvf.js
@@ -1,5 +1,5 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))a(r);new MutationObserver(r=>{for(const n of r)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function o(r){const n={};return r.integrity&&(n.integrity=r.integrity),r.referrerPolicy&&(n.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?n.credentials="include":r.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function a(r){if(r.ep)return;r.ep=!0;const n=o(r);fetch(r.href,n)}})();let s=[],l=[];async function c(){s=await(await fetch("./data.json")).json(),console.log(s),g()}function g(){const e=document.getElementById("jobs");e.innerHTML=d().map(t=>f(t)).join(""),u()}function d(){return l.length===0?s:s.filter(e=>{const t=[e.role,e.level,...e.languages,...e.tools];return l.every(o=>t.includes(o))})}function u(){const e=document.querySelector(".container"),t=document.querySelector(".filters");if(l.length===0){e.classList.add("hidden");return}e.classList.remove("hidden"),t.innerHTML=`
-        ${l.map(o=>`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))g(n);new MutationObserver(n=>{for(const l of n)if(l.type==="childList")for(const i of l.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&g(i)}).observe(document,{childList:!0,subtree:!0});function o(n){const l={};return n.integrity&&(l.integrity=n.integrity),n.referrerPolicy&&(l.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?l.credentials="include":n.crossOrigin==="anonymous"?l.credentials="omit":l.credentials="same-origin",l}function g(n){if(n.ep)return;n.ep=!0;const l=o(n);fetch(n.href,l)}})();let s=[],r=[];async function c(){s=await(await fetch("./data.json")).json(),console.log(s),a()}function a(){const e=document.getElementById("jobs");e.innerHTML=d().map(t=>p(t)).join(""),u()}function d(){return r.length===0?s:s.filter(e=>{const t=[e.role,e.level,...e.languages,...e.tools];return r.every(o=>t.includes(o))})}function u(){const e=document.querySelector(".container"),t=document.querySelector(".filters");if(r.length===0){e.classList.add("hidden");return}e.classList.remove("hidden"),t.innerHTML=`
+        ${r.map(o=>`
             <div class=" flex flex-row bg-green-50 rounded-lg">
                 <span class=" text-green-400 px-4 py-1 rounded-full font-bold">${o}</span>
                 <div class="w-8 h-8 bg-green-400 hover:bg-black transition-all duration-100 ease-in-out rounded-r-lg flex items-center justify-center">
@@ -11,7 +11,7 @@
                 </div>
             </div>
             `).join("")}
-    `}function p(){l=[],g()}function f(e){return`
+    `}window.toggleFilter=function(e){r.includes(e)?r=r.filter(t=>t!==e):r.push(e),a()};window.clearFilters=function(){r=[],a()};function p(e){return`
         <div class="bg-white rounded-lg shadow-xl transition-all duration-200 ease-in-out flex flex-col h-64.75 lg:h-38 lg:items-center lg:flex-row lg:w-full lg:justify-between lg:pt-0  relative px-4 pt-10 justify-items-start gap-2 md:pb-3 lg:pb-0 w-[clamp(336px,100%,360px)] ${e.featured?"border-l-6 border-green-400":""}"
         >
         <img src="${e.logo}" alt="${e.company}" class="transition-all duration-200 w-12 h-12 -top-5.75 rounded-full absolute left-[-16] lg:top-8 lg:left-6 lg:w-22 lg:h-22">
@@ -37,23 +37,23 @@
 
             <div class="flex gap-2 flex-wrap lg:flex-row lg:h-10">
                 <span onclick="toggleFilter('${e.role}')" class="
-                ${l.includes(e.role)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
+                ${r.includes(e.role)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
                  font-bold  px-4 py-2 rounded  transition-all duration-100 cursor-pointer">${e.role}</span>
                 <span onclick="toggleFilter('${e.level}')" class="
-                ${l.includes(e.level)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
+                ${r.includes(e.level)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
 
                  font-bold  px-4 py-2 rounded  transition-all duration-100 cursor-pointer">${e.level}</span>
                 ${e.languages.map(t=>`
                     <span onclick="toggleFilter('${t}')" class="
-                    ${l.includes(t)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
+                    ${r.includes(t)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
                      font-bold  px-4 py-2 rounded transition-all duration-100 cursor-pointer">${t}</span>
                     `).join("")}
                 ${e.tools.map(t=>`
                     <span onclick="toggleFilter('${t}')" class="
-                    ${l.includes(t)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
+                    ${r.includes(t)?"bg-green-400 text-green-50 hover:bg-green-400/80 hover:text-green-50":"text-green-400 bg-green-50 hover:bg-green-400 hover:text-green-50"}
                     font-bold  px-4 py-2 rounded transition-all duration-100 cursor-pointer">${t}</span>
                     `).join("")}
                 
             </div>
         </div>            
-    `}const x=document.querySelector(".clearButton");x.addEventListener("click",p);c();
+    `}const f=document.querySelector(".clearButton");f.addEventListener("click",clearFilters);c();
